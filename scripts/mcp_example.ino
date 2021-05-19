befcce0f8965386b0a1847654db9319829b2f143
TJ <tbruno25@gmail.com>
2021-05-19 15:05:01 -0700

    add arduino mcp example script

---
+++ scripts/mcp_example.ino
@@ -0,0 +1,49 @@
+/*
+example sketch to use arduino as CAN interface over serial with python-can / CanoPy
+https://github.com/Tbruno25/canopy
+
+mcp_can library | https://github.com/coryjfowler/MCP_CAN_lib
+default 500K baudrate / 8MHz clock speed
+
+don't forget to modify CanoPy config.py
+    interface = "serial"
+    channel = "COM1" #Windows OR "/dev/ttyACM0" #Linux 
+    bitrate = 115200
+*/
+
+#include <mcp_can.h>
+#include <SPI.h>
+
+long unsigned int rxId;
+unsigned char arrId[4];
+unsigned char len = 0;
+unsigned char rxBuf[8];
+const unsigned char blank[] = {0, 0, 0, 0};
+
+#define CAN0_INT 2 // Set INT to pin 2
+MCP_CAN CAN0(10);  // Set CS to pin 10
+
+void setup()
+{
+  CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ); // baudrate and clock speed
+  CAN0.setMode(MCP_NORMAL);                   // Set operation mode to normal so the MCP2515 sends acks to received data.
+  pinMode(CAN0_INT, INPUT);                   // Configuring pin for /INT input
+
+  delay(50);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  if (!digitalRead(CAN0_INT)) // If CAN0_INT pin is low, read receive buffer
+  {
+    CAN0.readMsgBuf(&rxId, &len, rxBuf); // read data
+
+    Serial.write(0xAA);                        // start of frame
+    Serial.write(blank, 4);                    // timestamp
+    Serial.write(len);                         // dlc
+    Serial.write((const uint8_t *)(&rxId), 4); // id
+    Serial.write(rxBuf, len);                  // payload
+    Serial.write(0xBB);                        // end of frame
+  }
+}
