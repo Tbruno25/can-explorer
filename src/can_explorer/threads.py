004c50fb069961861d19017ff23e6e49dfe25d4f
TJ <tbruno25@gmail.com>
2023-04-13 20:46:10 -0700

    Add stoppable thread

---
+++ src/can_explorer/threads.py
@@ -0,0 +1,22 @@
+import threading
+
+
+class StoppableThread(threading.Thread):
+    """
+    Basic thread that creates an event which can be implemented
+    in the run method to interrupt long running loops.
+
+    Example:
+        def run(self):
+            while not self.cancel.wait(5):
+                ...
+    """
+
+    def __init__(self, daemon=True):
+        super().__init__()
+        self.daemon = daemon
+        self.cancel = threading.Event()
+
+    def stop(self):
+        self.cancel.set()
+        self.join()
